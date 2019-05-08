7e65b2ece22cca4d93863223342c225bd8f4d1ad
Will1400 <will1400@edu.campusvejle.dk>
2019-05-08 11:34:33 +0200

    Edit Ingredient

---
+++ Web/Pages/Ingredients/Edit.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Ingredients
+{
+    public class EditModel : PageModel
+    {
+        IngredientRepository ingredientRepository = new IngredientRepository();
+
+        [BindProperty]
+        public Ingredient Ingredient { get; set; }
+
+        public void OnGet()
+        {
+            int.TryParse((string)RouteData.Values["Id"], out int id);
+            Ingredient = ingredientRepository.GetIngredient(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                ingredientRepository.UpdateIngredient(Ingredient);
+                return Redirect("/Ingredients/Index");
+            }
+            return Page();
+        }
+    }
+}
