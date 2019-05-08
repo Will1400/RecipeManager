43a448fdcc77952064ea515fe4a3860b8b15150b
Will1400 <will1400@edu.campusvejle.dk>
2019-05-08 11:54:26 +0200

    Recipes index

---
+++ Web/Pages/Recipes/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Recipes
+{
+    public class IndexModel : PageModel
+    {
+        RecipeRepository recipeRepository = new RecipeRepository();
+
+        public List<Recipe> Recipes { get; set; }
+
+        public void OnGet()
+        {
+            Recipes = recipeRepository.GetAllRecipes();
+        }
+        public IActionResult OnGetDelete()
+        {
+            int.TryParse((string)RouteData.Values["Id"], out int id);
+            recipeRepository.DeleteRecipe(id);
+            return Redirect("/Recipes/Index");
+        }
+    }
+}
