97a63b944c32636710376a51188649358521d32f
Will1400 <will1400@edu.campusvejle.dk>
2019-05-08 14:51:58 +0200

    Recipe Details page

---
+++ Web/Pages/Recipes/Details.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Entities;
+using DataAccess;
+
+namespace Web.Pages.Recipes
+{
+    public class DetailsModel : PageModel
+    {
+        RecipeRepository recipeRepository = new RecipeRepository();
+
+        public Recipe Recipe { get; set; }
+
+        public void OnGet()
+        {
+            int.TryParse((string)RouteData.Values["Id"], out int id);
+            Recipe = recipeRepository.GetRecipe(id);
+        }
+    }
+}
