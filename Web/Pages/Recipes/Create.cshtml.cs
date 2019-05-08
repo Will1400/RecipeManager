abec6fee7ac88de78270769c9f27aacc7a1eeeb7
Will1400 <will1400@edu.campusvejle.dk>
2019-05-08 12:30:34 +0200

    Create Recipe

---
+++ Web/Pages/Recipes/Create.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Recipes
+{
+    public class CreateModel : PageModel
+    {
+        RecipeRepository recipeRepository = new RecipeRepository();
+
+        [BindProperty]
+        public Recipe Recipe { get; set; }
+
+        public void OnGet() {}
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                recipeRepository.NewRecipe(Recipe);
+                return Redirect("/Recipes/Index");
+            }
+            return Page();
+        }
+    }
+}
