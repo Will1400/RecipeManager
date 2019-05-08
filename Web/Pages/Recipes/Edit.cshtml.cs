d1fe9b23f01dc82e917adeaf9f9deb1c514a96e7
Will1400 <will1400@edu.campusvejle.dk>
2019-05-08 14:40:11 +0200

    Edit page for recipes

---
+++ Web/Pages/Recipes/Edit.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Entities;
+using DataAccess;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Web.Pages.Recipes
+{
+    public class EditModel : PageModel
+    {
+        RecipeRepository recipeRepository = new RecipeRepository();
+        IngredientRepository ingredientRepository = new IngredientRepository();
+
+        public SelectList Ingredients { get; set; }
+
+        [BindProperty]
+        public Recipe Recipe { get; set; }
+        
+        [BindProperty]
+        public Ingredient NewIngredient { get; set; }
+
+
+        public void OnGet()
+        {
+            int.TryParse((string)RouteData.Values["Id"], out int id);
+            Recipe = recipeRepository.GetRecipe(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                recipeRepository.UpdateRecipe(Recipe);
+                return Redirect("/Recipes/Index");
+            }
+            return Page();
+        }
+    }
+}
