b56a9f0543948c5a4c463fc16b06f4de75fa05c7
Will1400 <will1400@edu.campusvejle.dk>
2019-05-08 09:32:41 +0200

    DataAccess setup

--- DataAccess/CommonRepository.cs
+++ DataAccess/CommonRepository.cs
@@ -1,10 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace DataAccess
 {
-    class CommonRepository
+    public class CommonRepository
     {
+        private string conString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RpRecipeManager;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public int ExecuteNonQuery(string q)
+        {
+            int rowsAffected = 0;
+
+            using (SqlConnection con = new SqlConnection(conString))
+            using (SqlCommand com = new SqlCommand(q, con))
+            {
+                con.Open();
+
+                rowsAffected = com.ExecuteNonQuery();
+            }
+            return rowsAffected;
+        }
+
+        public int ExecuteNonQueryScalar(string q)
+        {
+            int id = 0;
+            using (SqlConnection con = new SqlConnection(conString))
+            using (SqlCommand com = new SqlCommand(q, con))
+            {
+                con.Open();
+
+                id = (int)com.ExecuteScalar();
+            }
+            return id;
+        }
+
+        public DataTable ExecuteQuery(string q)
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection(conString))
+            using (SqlCommand com = new SqlCommand(q, con))
+            using (SqlDataAdapter dap = new SqlDataAdapter(com))
+            {
+                dap.Fill(ds);
+            }
+            return ds.Tables[0];
+        }
     }
 }
---
+++ DataAccess/Sql.sql
@@ -0,0 +1,20 @@
+﻿--create table Recipes(
+--	Id int not null primary key identity(1,1),
+--	Name varchar(max) not null,
+--	Description varchar(max)
+--);
+
+--create table Ingredients(
+--	Id int not null primary key identity(1,1),
+--	Name varchar(max) not null,
+--	Type int not null
+--);
+
+--create table IngredientsInRecipe(
+--	IngredientId int foreign key references Ingredients(Id),
+--	RecipeId int foreign key references Recipes(Id),
+--	Amount int,
+--	Unit varchar(max),
+--	constraint IIR_PK primary key (IngredientId, RecipeId)
+--);
+
