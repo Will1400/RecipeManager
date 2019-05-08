ce0fcf238fba11bb82df0debc51c09f888ca15ac
Will1400 <will1400@edu.campusvejle.dk>
2019-05-08 09:39:14 +0200

    Entities

---
+++ Entities/IngredientType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public enum IngredientType
+    {
+        Na,
+        Poultry,
+        Dairy,
+        Grain,
+        Pork,
+        Beef,
+        Seafood,
+        Vegetables,
+        Nuts,
+        Fruits,
+        Rootfruits
+    }
+}
---
+++ Entities/Recipe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Recipe
+    {
+        private List<Ingredient> ingredients;
+        private string description;
+        private string name;
+        private int id;
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public List<Ingredient> Ingredients
+        {
+            get { return ingredients; }
+            set { ingredients = value; }
+        }
+    }
+}
