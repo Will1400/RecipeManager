8fff8c5f0c9def7dee456125c54a1ffb2f733de7
Will1400 <will1400@edu.campusvejle.dk>
2019-05-08 14:48:39 +0200

    Home Page with recipes

--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Entities;
+using DataAccess;
 
 namespace Web.Pages
 {
     public class IndexModel : PageModel
     {
+        RecipeRepository recipeRepository = new RecipeRepository();
+
+        public List<Recipe> Recipes { get; set; }
+
         public void OnGet()
         {
-
+            Recipes = recipeRepository.GetAllRecipes();
         }
     }
 }
